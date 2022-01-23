d3f91316eb9e3cc710acae44c07d17ccff25c8de
E17762 <E17762@P21000249.banxico.org.mx>
2022-01-22 19:52:59 -0600

    creo el proyecto inicial

---
+++ src/test/java/mx/unam/dgtic/proyectofinal7/Proyectofinal7ApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.unam.dgtic.proyectofinal7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyectofinal7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
